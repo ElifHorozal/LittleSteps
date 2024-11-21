f94547907dcc89b4b5c6cf787fc3548229cb1f9e
ElifHorozal <trigelf@gmail.com>
2024-11-21 14:49:38 +0300

    added the side menu to necessary ones

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,19 +5,21 @@ import 'package:intl/intl.dart';
 import 'package:little_steps/models/add_habit_dialog.dart';
 import 'package:little_steps/models/habit_helper.dart';
 import 'package:little_steps/components/habit_progress_chart.dart';
+import 'package:little_steps/components/nav_drawer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
- final HabitHelper _habitHelper = HabitHelper();
+
+final HabitHelper _habitHelper = HabitHelper();
 
 class _HomeScreenState extends State<HomeScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  
+
   @override
   void initState() {
     super.initState();
@@ -25,98 +27,102 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   DateTime getStartOfWeek(DateTime date) {
-  return date.subtract(Duration(days: date.weekday - 1));
+    return date.subtract(Duration(days: date.weekday - 1));
   }
 
-  Future<void> _addHabit(String habitName, String chartType, Color customColor) async {
-  final user = FirebaseAuth.instance.currentUser;
-  if (user != null) {
-    final today = DateTime.now();
-    final weekStart = today.subtract(Duration(days: today.weekday - 1));
+  Future<void> _addHabit(
+      String habitName, String chartType, Color customColor) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final today = DateTime.now();
+      final weekStart = today.subtract(Duration(days: today.weekday - 1));
 
-    final habit = {
-      'name': habitName,
-      'chartType': chartType,
-      'createdAt': FieldValue.serverTimestamp(),
-      'weeklyProgress': List<bool>.generate(7, (_) => false),
-      'currentWeekStart': DateFormat('yyyy-MM-dd').format(weekStart),
-      'color': {
-        'red': customColor.red,
-        'green': customColor.green,
-        'blue': customColor.blue,
-      }, // Renk bilgisi kaydediliyor
-    };
+      final habit = {
+        'name': habitName,
+        'chartType': chartType,
+        'createdAt': FieldValue.serverTimestamp(),
+        'weeklyProgress': List<bool>.generate(7, (_) => false),
+        'currentWeekStart': DateFormat('yyyy-MM-dd').format(weekStart),
+        'color': {
+          'red': customColor.red,
+          'green': customColor.green,
+          'blue': customColor.blue,
+        }, // Renk bilgisi kaydediliyor
+      };
 
-    await FirebaseFirestore.instance
-        .collection('users')
-        .doc(user.uid)
-        .collection('habits')
-        .add(habit);
-  }
-}
-
-Future<void> _checkWeeklyProgressForAllHabits() async {
-  final user = FirebaseAuth.instance.currentUser;
-  if (user != null) {
-    final habits = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(user.uid)
-        .collection('habits')
-        .get();
-
-    for (var habit in habits.docs) {
-      await _checkAndResetWeeklyProgress(habit.id);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .collection('habits')
+          .add(habit);
     }
   }
-}
-Future<void> _checkAndResetWeeklyProgress(String habitId) async {
-  final user = _auth.currentUser;
-  if (user != null) {
-    final today = DateTime.now();
-    final weekStart = getStartOfWeek(today); // Haftanın Pazartesi'sini hesapla
 
-    final normalizedWeekStart = DateTime(weekStart.year, weekStart.month, weekStart.day); // Normalize
-    final habitRef = _firestore
-        .collection('users')
-        .doc(user.uid)
-        .collection('habits')
-        .doc(habitId);
+  Future<void> _checkWeeklyProgressForAllHabits() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final habits = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .collection('habits')
+          .get();
 
-    final habitSnapshot = await habitRef.get();
-    if (habitSnapshot.exists) {
-      final data = habitSnapshot.data() as Map<String, dynamic>;
-
-      // Firestore'dan alınan tarihi doğru formatta okuyun
-      final currentWeekStart = data['currentWeekStart'] is String
-          ? DateTime.parse(data['currentWeekStart'])
-          : (data['currentWeekStart'] as Timestamp).toDate();
-
-      final normalizedCurrentWeekStart = DateTime(
-        currentWeekStart.year,
-        currentWeekStart.month,
-        currentWeekStart.day,
-      ); // Normalize
-
-      print('Normalized Week start: $normalizedWeekStart');
-      print('Normalized Current week start: $normalizedCurrentWeekStart');
-
-      // Eğer hafta değişmişse sadece o zaman sıfırla
-      if (normalizedWeekStart.isAfter(normalizedCurrentWeekStart)) {
-        print('Hafta değişti, weeklyProgress sıfırlanıyor...');
-        await habitRef.update({
-          'weeklyProgress': List<bool>.generate(7, (_) => false), // Haftalık ilerleme sıfırlanır
-          'currentWeekStart': DateFormat('yyyy-MM-dd').format(normalizedWeekStart), // Yeni hafta başlangıç tarihi
-        });
-      } else {
-        print('Aynı hafta, sıfırlama yapılmıyor.');
+      for (var habit in habits.docs) {
+        await _checkAndResetWeeklyProgress(habit.id);
       }
     }
   }
-}
 
+  Future<void> _checkAndResetWeeklyProgress(String habitId) async {
+    final user = _auth.currentUser;
+    if (user != null) {
+      final today = DateTime.now();
+      final weekStart =
+          getStartOfWeek(today); // Haftanın Pazartesi'sini hesapla
 
+      final normalizedWeekStart =
+          DateTime(weekStart.year, weekStart.month, weekStart.day); // Normalize
+      final habitRef = _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('habits')
+          .doc(habitId);
 
- void _showAddHabitDialog(BuildContext context) {
+      final habitSnapshot = await habitRef.get();
+      if (habitSnapshot.exists) {
+        final data = habitSnapshot.data() as Map<String, dynamic>;
+
+        // Firestore'dan alınan tarihi doğru formatta okuyun
+        final currentWeekStart = data['currentWeekStart'] is String
+            ? DateTime.parse(data['currentWeekStart'])
+            : (data['currentWeekStart'] as Timestamp).toDate();
+
+        final normalizedCurrentWeekStart = DateTime(
+          currentWeekStart.year,
+          currentWeekStart.month,
+          currentWeekStart.day,
+        ); // Normalize
+
+        print('Normalized Week start: $normalizedWeekStart');
+        print('Normalized Current week start: $normalizedCurrentWeekStart');
+
+        // Eğer hafta değişmişse sadece o zaman sıfırla
+        if (normalizedWeekStart.isAfter(normalizedCurrentWeekStart)) {
+          print('Hafta değişti, weeklyProgress sıfırlanıyor...');
+          await habitRef.update({
+            'weeklyProgress': List<bool>.generate(
+                7, (_) => false), // Haftalık ilerleme sıfırlanır
+            'currentWeekStart': DateFormat('yyyy-MM-dd')
+                .format(normalizedWeekStart), // Yeni hafta başlangıç tarihi
+          });
+        } else {
+          print('Aynı hafta, sıfırlama yapılmıyor.');
+        }
+      }
+    }
+  }
+
+  void _showAddHabitDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (context) {
@@ -124,12 +130,13 @@ Future<void> _checkAndResetWeeklyProgress(String habitId) async {
       },
     );
   }
-  
- @override
+
+  @override
   Widget build(BuildContext context) {
     final user = _auth.currentUser;
 
     return Scaffold(
+      drawer: NavDrawer(),
       appBar: AppBar(
         title: Text('Alışkanlıklarım'),
       ),
@@ -172,10 +179,10 @@ Future<void> _checkAndResetWeeklyProgress(String habitId) async {
                     final List<bool> weeklyProgress =
                         List<bool>.from(data['weeklyProgress']);
                     final Map<String, int> colorMap = {
-                        'red': data['color']['red'] ?? 0,
-                        'green': data['color']['green'] ?? 0,
-                        'blue': data['color']['blue'] ?? 0,
-                      };
+                      'red': data['color']['red'] ?? 0,
+                      'green': data['color']['green'] ?? 0,
+                      'blue': data['color']['blue'] ?? 0,
+                    };
                     final today = DateTime.now();
                     final currentDayIndex = today.weekday - 1;
 
@@ -189,11 +196,12 @@ Future<void> _checkAndResetWeeklyProgress(String habitId) async {
                             subtitle: HabitProgressChart(
                               chartType: chartType,
                               weeklyProgress: weeklyProgress,
-                              colors : colorMap,
+                              colors: colorMap,
                             ),
                           ),
                           Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
@@ -202,8 +210,8 @@ Future<void> _checkAndResetWeeklyProgress(String habitId) async {
                                   value: weeklyProgress[currentDayIndex],
                                   onChanged: (bool? value) async {
                                     if (value != null) {
-                                      await _habitHelper.updateDailyProgress(habit.id, value);
- 
+                                      await _habitHelper.updateDailyProgress(
+                                          habit.id, value);
                                     }
                                   },
                                 ),
